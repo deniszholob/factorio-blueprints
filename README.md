59cb4d3476e9fbcc72beff28c0be95fb3d7c4b3c
Denis Zholob <dddgamer@live.com>
2019-07-01 17:29:19 -0400

    Add website files.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# factorio-blueprints
-Collection of Personal Blueprints for Factorio Game
+# Factorio [Blueprints](https://deniszholob.github.io/factorio-blueprints)
+Collection of [Personal Blueprints](https://deniszholob.github.io/factorio-blueprints/) for [Factorio](https://www.factorio.com/) Game
+
+# Check out my other Factorio Projects
+* [Factorio Cheat Sheet](https://factoriocheatsheet.com/)
+* [Factorio Ratio Calculator](https://deniszholob.github.io/factorio-machine-ratio-calc/)
+* [Factorio Softmod Collection](https://github.com/deniszholob/factorio-softmod-pack)
+
+
+# Support Me
+If you found the blueprints or the source code useful, consider supporting me on Patreon.
+
+[![button](https://c5.patreon.com/external/logo/downloads_wordmark_white_on_coral.png)](https://www.patreon.com/deniszholob)
+
---
+++ src/js/blueprints.js
@@ -0,0 +1,32 @@
+const blueprintData = [
+  {
+    "name": "Smelting",
+    "book": true,
+    "url": "https://gist.github.com/dddgamer/326acde2fbcc604e9affa78c4c9e211d",
+    "raw": "https://gist.githubusercontent.com/dddgamer/326acde2fbcc604e9affa78c4c9e211d/raw/5b7b4de0eb2d180fe478bb7b87834dfdc0eaa5cd/DDD-Smelting.txt"
+  },
+  {
+    "name": "Steam Power",
+    "book": true,
+    "url": "https://gist.github.com/dddgamer/3ddc407862874e3b734062f73f9434c1",
+    "raw": "https://gist.githubusercontent.com/dddgamer/3ddc407862874e3b734062f73f9434c1/raw/cc43d9b6183cd805caa9f742edb2a88443a0a7d6/DDD-Steam-Power.txt"
+  },
+  {
+    "name": "Rails 2 Lane RHD (MojoD's Remix)",
+    "book": true,
+    "url": "https://gist.github.com/dddgamer/9c752d15a99f0d336da71686d0b3b8e0",
+    "raw": "https://gist.githubusercontent.com/dddgamer/9c752d15a99f0d336da71686d0b3b8e0/raw/e36e1ab02309be409109c3db7664faaa4e790953/DDD-Rail-2-Lane-(RHD)-%255BMojoD%2520Remix%255D.txt"
+  },
+  {
+    "name": "Rails 2 Lane RHD Decorations",
+    "book": true,
+    "url": "https://gist.github.com/dddgamer/9ccb73bddbb90d4bc01682b66aac5dec",
+    "raw": "https://gist.github.com/dddgamer/9c752d15a99f0d336da71686d0b3b8e0"
+  },
+  {
+    "name": "",
+    "book": true,
+    "url": "",
+    "raw": ""
+  },
+]
