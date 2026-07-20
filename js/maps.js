b80baa08e83b641fd2e1fe4a3983a84b148c289b
Denis Zholob <dddgamer3x@gmail.com>
2026-07-20 16:49:52 -0600

    Add SA-2.1_11 map

--- js/maps.js
+++ js/maps.js
@@ -3,7 +3,7 @@ export const mapData = [
     "name": "SA-2.1_11",
     "folder": "SA-2.1_11",
     "version": "v2.1.11",
-    "url": "./maps/SA-2.1_11/index.html",
+    "url": "./maps/SA-2.1_11/",
     "thumbnail": "./maps/SA-2.1_11/thumbnail.png",
     "tags": "RateCalculator, creative-mod, elevated-rails, flib, mapshot, quality, recycler, science-tab, space-age"
   }
